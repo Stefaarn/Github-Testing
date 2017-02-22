5f70ae7cda5acf3a62a372eaa430a3054209a591
Stefaarn <s.bredahl@hotmail.com>
2017-02-22 12:03:39 +0100

    Progress

--- Github - Testing/Github - Testing/Program.cs
+++ Github - Testing/Github - Testing/Program.cs
@@ -13,6 +13,7 @@ namespace Github___Testing
             Console.WriteLine("First Commit");
             Console.WriteLine("Second Commit fra egen branch");
             Console.WriteLine("Push?");
+            Console.WriteLine("This git is weird");
         }
     }
 }
