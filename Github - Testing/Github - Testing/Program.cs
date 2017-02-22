9727da17dddbaf619913023ccd6d090f90f2cfa5
Stefaarn <s.bredahl@hotmail.com>
2017-02-22 11:05:43 +0100

    Add files via upload

--- Github - Testing/Github - Testing/Program.cs
+++ Github - Testing/Github - Testing/Program.cs
@@ -11,6 +11,7 @@ namespace Github___Testing
         static void Main(string[] args)
         {
             Console.WriteLine("First Commit");
+            Console.WriteLine("Second Commit fra egen branch");
         }
     }
 }
